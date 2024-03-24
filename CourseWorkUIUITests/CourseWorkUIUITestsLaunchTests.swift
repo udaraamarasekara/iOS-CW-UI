0546a6cfee14ba6c2f24896846ac62993075cd38
udaraamarasekara <udaraprabath98@gmail.com>
2024-03-24 01:45:04 -0700

    Initial Commit

---
+++ CourseWorkUIUITests/CourseWorkUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CourseWorkUIUITestsLaunchTests.swift
+//  CourseWorkUIUITests
+//
+//  Created by Udara PC on 2024-03-24.
+//
+
+import XCTest
+
+final class CourseWorkUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
